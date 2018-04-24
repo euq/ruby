e666118bb224504daf9edc28a3aa7bfecae1b03f
euq <choco.87.nano@gmail.com>
2018-04-24 11:36:55 +0900

    add file

--- abc095c.rb
+++ abc095c.rb
@@ -4,5 +4,21 @@
  2ab = x + y
  '''
 
-if  c < a + b
+a,b,c,x,y = gets.chomp!.split(" ").map(&:to_i)
+cost = Array.new
 
+hash={x => a, y => b }
+price = [a, b]
+hm = [x, y]
+#p price
+#p howmany.min
+#p howmany.max
+if c * 2 > a + b
+  cost[0] = a * x + b * y
+else
+  cost[0] = 2*c*hm.min + hash[hm.max]*(hm.max-hm.min)
+  cost[1] = 2*c*hm.max
+#  p cost
+end
+
+p cost.min
